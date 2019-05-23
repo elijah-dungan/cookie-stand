8bd4a7b0d17f161b62bf668a5306eece23d06d27
root <eli.dungan@gmail.com>
2019-05-23 01:31:01 -0700

    added glass-like table

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ var tableBodyEl = document.getElementById('sales-table'); // stores reference to
 /* --Global Arrays-- */
 
 var times = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm']; // array of times from 6am to 8pm
+var allStores = [];
 var allSales = []; // stores all cookie sold per hour from all stores into one array
 var hourlyTotals = []; // stores totals per hour across stores
 
@@ -102,6 +103,7 @@ Store.prototype.renderTableData = function() { // renders table data to the DOM
   tableDataEl.textContent = locationTotal; // adds totalCookiesSold to the table data
   tableRowEl.appendChild(tableDataEl); // appends the table data to the table row
   allSales.push(this.cookiesSoldPerHour); // stores cookiesSoldPerHour into a global sales array
+  allStores.push(this);
   console.log(`locationTotal: ${locationTotal}`);
 };
 
