2065ef816330c2e4350c3c3b6102f5deaf8f0d32
root <eli.dungan@gmail.com>
2019-05-22 17:47:59 -0700

    fully functioning table

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 'use strict';
 
+/* --Global Variables */
+
+var tBodyEl = document.getElementById('sales-table');
+
 /* --Global Arrays-- */
 
+var times = ['6am:', '7am:', '8am:', '9am:', '10am:', '11am:', '12pm:', '1pm:', '2pm:', '3pm:', '4pm:', '5pm:', '6pm:', '7pm:', '8pm:']; // array of times from 6am to 8pm
 var allStores = [];
 var allSales = [];
 
@@ -21,64 +26,94 @@ function makeRandom(minCust, maxCust) { // gets random number from a range of tw
   return Math.floor(Math.random() * (maxCust - minCust + 1)) + minCust;
 }
 
-function printList(inputParentEl, outputChildEl, inputTextContent) { // creates a child element and inputs text within the child element based on an element ID, tag name of child, and input text
-  var getParentEl = document.getElementById(inputParentEl); // gets parent element by ID
-  var newChildEl = document.createElement(outputChildEl); // creates child element
-  newChildEl.textContent = inputTextContent; // adds text to the child element
-  getParentEl.appendChild(newChildEl); // appends the child element to the parent
-}
-
-function StoreByLocation(elementId, minCustPerHour, maxCustPerHour, averageCookieSale) {
-  this.elementId = elementId;
+function Store(name, minCustPerHour, maxCustPerHour, averageCookieSale) {
+  this.name = name;
   this.minCustPerHour = minCustPerHour;
   this.maxCustPerHour = maxCustPerHour;
   this.averageCookieSale = averageCookieSale;
-  this.times = ['6am:', '7am:', '8am:', '9am:', '10am:', '11am:', '12pm:', '1pm:', '2pm:', '3pm:', '4pm:', '5pm:', '6pm:', '7pm:', '8pm:']; // array of times from 6am to 8pm
   this.cookiesSoldPerHour = []; // stores cookies sold per hour
-  this.listPerHour = [];
+}
+
+function renderTableHeader() {
+
+  tBodyEl;
+  var trEl = document.createElement('tr');
+  tBodyEl.appendChild(trEl);
+
+  var thEl = document.createElement('th');
+  thEl.textContent = 'Name';
+  trEl.appendChild(thEl);
+
+  for(var i = 0; i < times.length; i ++) {
+    var thEl = document.createElement('th');
+    thEl.textContent = times[i];
+    trEl.appendChild(thEl);
+  }
+
+  var thEl = document.createElement('th');
+  thEl.textContent = 'Total';
+  trEl.appendChild(thEl);
+
 }
 
 /* --Functions-- */
 
 /* --Prototypes-- */
 
-StoreByLocation.prototype.render = function() { // generates a list of random cookies based on a parent element ID and 3 numbers
-  console.log(`elementId: ${this.elementId}, minCustPerHour: ${this.minCustPerHour}, maxCustPerHour: ${this.maxCustPerHour}, averageCookieSale: ${this.averageCookieSale}`);
-  for(var i = 0; i < this.times.length; i ++) { // for loop that generates simulated sales and pushes into salesArray
+Store.prototype.renderTableData = function() { // generates a list of random cookies based on a parent element ID and 3 numbers
+  console.log(`elementId: ${this.storeLocation}, minCustPerHour: ${this.minCustPerHour}, maxCustPerHour: ${this.maxCustPerHour}, averageCookieSale: ${this.averageCookieSale}`);
+
+  tBodyEl;
+  var trEl = document.createElement('tr');
+  tBodyEl.appendChild(trEl);
+
+  var tdEl = document.createElement('td');
+  tdEl.textContent = this.name;
+  trEl.appendChild(tdEl);
+
+  for(var i = 0; i < times.length; i ++) { // for loop that generates simulated sales and pushes into salesArray
     var randomNum = makeRandom(this.minCustPerHour, this.maxCustPerHour);
     this.cookiesSoldPerHour.push(Math.round(randomNum * this.averageCookieSale)); // gets randomly generated number from minCustPerHour and maxCustPerHour, multiplies this number by meanCookieSale, pushes number into salesArray
-    var salesList = `${this.times[i]} ${this.cookiesSoldPerHour[i]} cookies`; // creates string with time, number of sales, and number of cookies sold
-    this.listPerHour.push(salesList);
-    printList(this.elementId, 'li', salesList); // takes strings and displays it inside child li elements
+
+    tdEl = document.createElement('td');
+    tdEl.textContent = this.cookiesSoldPerHour[i];
+    trEl.appendChild(tdEl);
+
     console.log(`randomNum: ${randomNum}, cookiesSoldPerHour: ${this.cookiesSoldPerHour[i]}`);
   }
-  var listTotal = `Total: ${this.cookiesSoldPerHour.reduce(getSum)} cookies`; // creates string with total number of cookies sold
-  printList(this.elementId, 'li', listTotal); // takes strings and displays it inside child li elements
+
+  var total = this.cookiesSoldPerHour.reduce(getSum);
+
+  tdEl = document.createElement('td');
+  tdEl.textContent = total;
+  trEl.appendChild(tdEl);
+
   allSales.push(this.cookiesSoldPerHour); // stores salesArray into an array
   allStores.push(this); // stores instance into an array
-  console.log(`listPerHour: ${this.listPerHour}`);
-  console.log(`listTotal: ${listTotal}`);
+
+  console.log(`listTotal: ${total}`);
 };
 
 /* --Prototypes-- */
 
 /* --Instances-- */
 
-var firstAndPike = new StoreByLocation('first-and-pike', '23', '65', '6.3');
-var seaTacAirport = new StoreByLocation('sea-tac-airport', '3', '24', '1.2');
-var seattleCenter = new StoreByLocation('seattle-center', '11', '38', '3.7');
-var capitolHill = new StoreByLocation('capitol-hill', '20', '38', '2.3');
-var alki = new StoreByLocation('alki', '2', '16', '4.6');
+var firstAndPike = new Store('First and Pike', '23', '65', '6.3');
+var seaTacAirport = new Store('Sea Tac Airport', '3', '24', '1.2');
+var seattleCenter = new Store('Seattle Center', '11', '38', '3.7');
+var capitolHill = new Store('Capitol Hill', '20', '38', '2.3');
+var alki = new Store('Alki', '2', '16', '4.6');
 
 /* --Instances-- */
 
 /* --Method Calls-- */
 
-firstAndPike.render();
-seaTacAirport.render();
-seattleCenter.render();
-capitolHill.render();
-alki.render();
+renderTableHeader();
+firstAndPike.renderTableData();
+seaTacAirport.renderTableData();
+seattleCenter.renderTableData();
+capitolHill.renderTableData();
+alki.renderTableData();
 
 /* --Method Calls-- */
 
